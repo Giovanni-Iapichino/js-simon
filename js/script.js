77a39baae5bafc38ad3c95c6492822a0cf916671
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-11 15:43:16 +0200

    add-function

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@
 //todo BONUS:
 //todo - Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
 //todo - Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.
+
+//* RECUPERO ELEMENTI DAL DOM
+const formEl = document.getElementById(`answers-form`);
+const inputEl = document.getElementById(`input-group`);
+const countdownEl = document.getElementById(`countdown`);
+const numberListEl = document.getElementById(`numbers-list`);
+const messageEl = document.getElementById(`message`);
+const instructionEl = document.getElementById(`instruction`);
+const buttonEl = document.querySelector(`button`);
+
+//* DEFINIZIONE VARIABILI
+const min = 1;
+const max = 99;
+const totalNumbers = 5;
+let time = 30;
+let numbers;
+let li = ``;
+
+//* INIZIALIZZAZIONE DELLE FUNZIONI
+const generateRandomNumbers = (min, max, tot) => {
+  numbers = [];
+  for (let i = 0; i < tot; i++) {
+    let randomNumbers = Math.floor(Math.random() * (max - min + 1)) + min;
+    numbers.push(randomNumbers);
+  }
+  return numbers;
+};
